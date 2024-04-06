0c49a1f876bdfafe013faf536b207c7690930afa
JNox <antifry@gmail.com>
2024-04-06 18:38:44 +0300

    Init

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+O
+src/main/webapp/WEB-INF/web.xml,2/e/2e22b6edd746fa3dc40ad7276c87c8d87d72a392
+Q
+!src/main/java/words/getWords.java,7/4/744bc6d43675e320e61f452593d510304b70cfd3
+P
+ src/main/java/words/getWord.java,9/7/97cab70189ae80fef54501e548add25d99537bd0
---
+++ src/main/java/words/getWord.java
@@ -0,0 +1,24 @@
+package words;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class getWord extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println(" SimpleServlet Executed");
+        out.flush();
+        out.close();
+
+    }
+}
